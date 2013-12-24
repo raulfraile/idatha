88c22cf7ecc36f689c680ca599e1302025b8ba4c
Raul Fraile <raulfraile@gmail.com>
2013-12-24 03:00:41 +0100

    Refactor namespaces

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$set = new Idatha\Set\Set();
+$set = new Idatha\Set();
 
 
 $element1 = new stdClass(1);
@@ -16,7 +16,7 @@ $set->add($element2);
 
 ld($set->isMember($element1), $set->isMember($element2), $set->isMember(new stdClass(1)));
 
-$set2 = new Idatha\Set\Set();
+$set2 = new Idatha\Set();
 
 $set2->add($element2);
 $set2->add($element3);
@@ -30,7 +30,7 @@ ld($set->isSubset($set2));
 
 
 ld($set->isMember($element1), $set->isMember($element2), $set->isMember(new stdClass(1)));
-$subset = new Idatha\Set\Set();
+$subset = new Idatha\Set();
 $subset->add($element1);
 $subset->add($element2);
 
--- ints.php
+++ ints.php
@@ -2,20 +2,20 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$set = new Idatha\Set\Set();
+$set = new Idatha\Set();
 
 $set->add(1);
 $set->add(2);
 
 ld($set->isMember(1), $set->isMember(2), $set->isMember(3));
 
-$set2 = new Idatha\Set\Set();
+$set2 = new Idatha\Set();
 $set2->add(1);
 $set2->add(2);
 
 ld($set->isSubset($set2), $set->isSubset($set2, true));
 
-$set3 = new Idatha\Set\Set();
+$set3 = new Idatha\Set();
 $set3->add(2);
 $set3->add(3);
 $set3->add(4);
